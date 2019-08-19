80c1415c289720313452ed749c04035e1d75e2bf
fatemeh-al <46268046+fatemeh-al@users.noreply.github.com>
2019-08-19 10:53:08 +0430

    Update lojic.js

--- signUpLogIn/lojic.js
+++ signUpLogIn/lojic.js
@@ -1,6 +1,8 @@
 function openSignupBox(){
     var signupBox = document.getElementsByClassName("box-signup")[0];
     var loginBox = document.getElementsByClassName("box-login")[0];
+    var triangle = document.getElementsByClassName("triangle")[0];
+    triangle.style.top = "55%";
     signupBox.style.display = "block";
     loginBox.style.display = "none";
 }
@@ -8,6 +10,8 @@ function openSignupBox(){
 function openLoginBox(){
     var signupBox = document.getElementsByClassName("box-signup")[0];
     var loginBox = document.getElementsByClassName("box-login")[0];
+    var triangle = document.getElementsByClassName("triangle")[0];
+    triangle.style.top = "50%";
     signupBox.style.display = "none";
     loginBox.style.display = "block";
 }
